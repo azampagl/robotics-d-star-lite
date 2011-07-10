d3c3706d6783ff923c7660785f2cd6ecd10370be
Aaron Zampaglione <azampagl@gmail.com>
2011-07-10 11:48:25 -0400

    Program updated to take command line arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "simulator.h"
 
 int main(int argc, char **argv)
 {
-	//FILE * log = fopen("log.txt","w");
+	if (argc != 9)
+		throw;
+
 	Simulator::Config config = Simulator::Config();
 
-	config.real_bitmap = "../../../../maps/map-01-real.bmp";
-	//config.robot_bitmap = "../../../../maps/map-01-robot.bmp";
-	config.robot_bitmap = "../../../../maps/map-01-robot.bmp";
+	config.real_bitmap = argv[2];
+	config.robot_bitmap = argv[3];
 
-	config.start.first = 0;
-	config.start.second = 0;
+	config.start.first = atoi(argv[4]);
+	config.start.second = atoi(argv[5]);
 
-	config.goal.first = 499;
-	config.goal.second = 499;
+	config.goal.first = atoi(argv[6]);
+	config.goal.second = atoi(argv[7]);
 
-	//config.goal.first = 19;
-	//config.goal.second = 19;
+	config.scan_radius = atoi(argv[8]);
 
-	config.scan_radius = 50;
-
-	Simulator sim = Simulator("Simulator 1", config);
+	Simulator sim = Simulator(argv[1], config);
 
 	sim.draw();
 
-	/*Map map(10, 10);
-
-	Map::Cell* start = map(0, 0);
-	Map::Cell* goal = map(9, 9);
-
-	Planner planner(&map, start, goal);
-
-	planner.replan();
-
-	list<Map::Cell*> path = planner.path();
-
-	for (list<Map::Cell*>::iterator i = path.begin(); i != path.end(); i++)
-	{
-		fprintf(log, "%d, %d\n", (*i)->x(), (*i)->y());
-	}
-
-	for (int i = 0; i < map.rows(); i++)
-	{
-		for (int j = 0; j < map.cols(); j++)
-		{
-			fprintf(log, "%.2f\n", map(i, j)->cost);
-		}
-	}
-
-	fclose(log);*/
 	return 0;
 }
